9a50c28661a498b268e231f24145f375fa9ec18e
Duy Nghia Tran <duynghia.tran@dataiku.com>
2024-10-23 12:52:14 +0200

    Initial commit from Create Next App

---
+++ eventcatalog.config.js
@@ -0,0 +1,27 @@
+/** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
+export default {
+  title: 'EventCatalog',
+  tagline: 'Discover, Explore and Document your Event Driven Architectures',
+  organizationName: 'Dataiku',
+  homepageLink: 'https://eventcatalog.dev/',
+  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: '/logo.png',
+    text: 'EventCatalog'
+  },
+  docs: {
+    sidebar: {
+      // Should the sub heading be rendered in the docs sidebar?
+      showPageHeadings: true
+    }
+  },
+  // required random generated id used by eventcatalog
+  cId: '54de36ab-d7ab-498b-81b3-229040c92933'
+}
